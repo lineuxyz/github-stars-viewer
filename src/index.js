44c68f4c1b98e81cef6e1f8b5950a489476a033d
Lineu Pastorelli <lineualberto@gmail.com>
2019-09-11 16:18:13 -0300

    added part of directions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justfyContent: 'center',
-    alingItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Bem vindo</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />;
+    </>
   );
 }
